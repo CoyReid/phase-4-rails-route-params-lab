0e37bf8aa0ed14b06fe9a25399fdef97afc4d84d
Coy Reid <coyreid7@gmail.com>
2021-09-19 17:59:28 -0500

    commit tree c3dc43726d12fababb1b7ee15bf4347f2e7b8d9c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = 
+    if params[:name]
+      Student.all.select {|student| student.last_name == params[:name].capitalize || student.first_name == params[:name].capitalize}
+    else
+      Student.all
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
